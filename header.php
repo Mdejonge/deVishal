6262f1355088a95cbd71e04e1a58b25c7f3ab349
Susanne <sjmprick@gmail.com>
2017-01-05 10:24:35 +0100

    Een of andere button uitgecomment

--- header.php
+++ header.php
@@ -51,12 +51,12 @@
 	<nav class="navbar navbar-default navbar-static-top" role="navigation">
 		<div class="container">
 			<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
+				<!--<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
 					<span class="sr-only">Toggle navigation</span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
-				</button>
+				</button>-->
 				<a class="navbar-brand" href="#"><img src="images/logo_vishal.png" id="logo"></a>
 			</div>
 			<div class="collapse navbar-collapse" id="navbar-collapse-1">
