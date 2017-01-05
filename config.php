17a3a05fd673e0f496188b1219a78a042975de47
Mdejonge <mennodejonge94@gmail.com>
2017-01-05 19:45:29 +0100

    Revert "Revert "Merge branch 'master' of C:\xampp\htdocs\deVishal with conflicts.""
    This reverts commit ce03abec04734acc6fdbf873d658b054de2b54ab.

--- config.php
+++ config.php
@@ -1,31 +1,32 @@
 <?php
+
 // NIET AAN DEZE GEGEVENS ZITTEN!!
 if($_SERVER['SERVER_NAME'] == '194.171.20.107')
 {
-    define("HOST", 'localhost');
-    define("USER", 'root');
-    define("PASSWORD", 'groep5Password');
-    define("DATABASE", 'devishal');
+    echo '<base href="//194.171.20.107/" />';
 
-    define("ROOT", '//194.171.20.107/');
+
+    // Database connectie
+    $conn = new mysqli('localhost', 'root', 'groep5Password', 'devishal');
+    if ($conn->connect_error) {
+        die('DB-verbinding mislukt '.$conn->connect_error);
+    }
+    mysqli_set_charset($conn,'utf8');
 }
 
 // DEZE GEGEVENS MOGEN/MOETEN VERANDERD WORDEN NAAR JE EIGEN LOKALE GEGEVENS.
 // DAARNA NIET UPLOADEN NAAR GITHUB AUB
 elseif($_SERVER['SERVER_NAME'] == 'localhost')
 {
-
-    define("HOST", '127.0.0.1');
-    define("USER", 'root');
-    define("PASSWORD", 'usbw');
-    define("DATABASE", 'devishal');
-
     // De link die staat na localhost:8080/ Bij mij staat er bijv localhost:8080/deVishal/index.php, dan moet je het
     // stukje wat bij mij /deVishal/ is, hier plaatsen
-    define("ROOT", '/deVishal/');
+    echo '<base href="/deVishal/" />';
+
+    // Database connectie
+    $conn = new mysqli('127.0.0.1', 'root', '', 'devishal');
+    if ($conn->connect_error) {
+        die('DB-verbinding mislukt '.$conn->connect_error);
+    }
+    mysqli_set_charset($conn,'utf8');
 }
-
-
-
-
 ?>
