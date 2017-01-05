ae61729a2667689daea720b68ec352b704161af6
Susanne <sjmprick@gmail.com>
2017-01-05 16:43:59 +0100

    dbconnect

--- dbconnect.php
+++ dbconnect.php
@@ -5,4 +5,14 @@ $conn = new mysqli(HOST, USER, PASSWORD, DATABASE);
         die('DB-verbinding mislukt '.$conn->connect_error);
     }
     mysqli_set_charset($conn,'utf8');
+
+function get_result($query){
+    global $conn;
+    $result = mysqli_query($conn, $query);
+    if (mysqli_num_rows($result)>0) {
+        return $result;
+    }
+
+    return false;
+}
 ?>
