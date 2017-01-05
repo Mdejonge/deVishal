fb5e0abb4f3291dfdade835176ab963b4ab36a87
wvpm <wvpm@hotmail.com>
2017-01-06 00:32:10 +0100

    Klaar met temlate uit DB halen

--- functions.php
+++ functions.php
@@ -71,77 +71,31 @@ function toon_bericht($naam, $startdatum, $foto_link) {
 }
 
 function toon_pagina($nummer) {
-    global $conn;
-    if ($conn->connect_error) {
-        die('DB-verbinding mislukt '.$conn->connect_error);
-    }
-    mysqli_set_charset($conn,'utf8');
-    $query = 'SELECT titel, tekst, sponsorfooter, templateId FROM pagina WHERE paginaId = '.$nummer;
-    $result = $conn->query($query);
-    if ($result->num_rows > 0) {
-        while($rec = $result->fetch_assoc()) {
-            geef_html($rec['templateId'], $rec['titel'], $rec['tekst']);
-            if ($rec['sponsorfooter']=1) {
-                include 'sponsors_block.php';
-            }
-        }
-    }
-    else {
-        echo  'Helaas, geen gegevens gevonden.';
-    }
-    mysqli_close($conn);
+	global $conn;
+	if ($conn->connect_error) {
+		die('DB-verbinding mislukt '.$conn->connect_error);
+	}
+	mysqli_set_charset($conn,'utf8');
+	$query = 'SELECT titel, tekst, sponsorfooter, layout FROM pagina INNER JOIN template ON pagina.templateId = template.templateId WHERE paginaId = '.$nummer;
+	$result = $conn->query($query);
+	if ($result->num_rows > 0) {
+		while($rec = $result->fetch_assoc()) {
+			geef_html($rec['titel'], $rec['tekst'], $rec['layout']);
+			if ($rec['sponsorfooter']=1) {
+				include 'sponsors_block.php';
+			}
+		}
+	}
+	else {
+		echo  'Helaas, geen gegevens gevonden.';
+	}
+	mysqli_close($conn);
 }
 
-function geef_html($templateId, $titel, $tekst) {
-    $html = '';
-    switch ($templateId) {
-        case 1:
-            $html = 'template1';
-            break;
-        case 2:
-            $html = '<h1>'.$titel.'</h1></br><div>'.$tekst.'</div>';
-            break;
-        case 3:
-            $html = 'template3';
-            break;
-        case 4:
-            $html = template_4($titel, $tekst);
-            break;
-        case 5:
-            $html = 'template5';
-            break;
-        default:
-            $html = '<h1>'.$titel.'</h1></br><div>'.$tekst.'</div>';
-    }
-    echo $html;
-}
-
-function template_4($titel, $tekst) {
-    echo ' 
-    <!-- Begin toppage -->
-  <div class="row toppage">
-  </div>
-  <div class="row kopjerow">
-    <div class="container">
-      <div class="col-md-12">
-        <div class="kopje-content">
-          <h4>
-              '.$titel.'
-          </h4>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- End toppage -->
- <!-- Begin content -->
-    <div class="row content">
-    <div class="container">
-
-        <h3>'.$titel.'</h3>
-        <div>'.$tekst.'</div>
-      </div>
-  </div>
-  <!-- End content -->';
+function geef_html($titel, $tekst, $layout) {
+	$layout = str_replace ('\'.$titel.\'', $titel, $layout);
+	$layout = str_replace ('\'.$tekst.\'', $tekst, $layout);
+	echo $layout;
 }
 
 ?>
