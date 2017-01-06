72a96952de0ce50df294cb801083577a31cb723f
wvpm <wvpm@hotmail.com>
2017-01-06 11:55:33 +0100

    toon_lid niet meer nodig

--- functions.php
+++ functions.php
@@ -1,10 +1,6 @@
 <?php
 include_once 'config.php';
 
-function toon_lid($voornaam, $achternaam, $website) {
-    return '<li><a href="', $website, '" target="_blank">', $voornaam, ' ', $achternaam, '</a></li>';
-}
-
 function toon_vrijwilliger($voornaam, $achternaam, $foto_link, $tekst) {
     $output =
         '<li>
