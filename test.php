3b90fc90be036301d49ab05ff70c3a28f4dd56c3
Susanne <sjmprick@gmail.com>
2017-01-09 13:43:12 +0100

    test.php dynamisch gemaakt

--- test.php
+++ test.php
@@ -1,7 +1,12 @@
 <?php
 
+if(isset($_GET['page_id']))
+    $page_id = $_GET['page_id'];
+else
+    $page_id = 1;
+
 require 'header.php';
-toon_pagina(5);
+toon_pagina($page_id);
 require 'footer.php';
 
 ?>
